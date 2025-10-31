e9361a35b3fe517d3f339cdcff1b3586f620159c
Alif Triadi <aliftriadi.gaming@gmail.com>
2025-10-31 15:23:54 +0700

    feat(user): add active navigation icon

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -18,7 +18,8 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::Users;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
+    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::Users;
 
     public static function form(Schema $schema): Schema
     {
