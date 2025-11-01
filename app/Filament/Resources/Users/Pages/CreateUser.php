c7141f807b8aa84f66998e519feca11ccd86385a
Alif Triadi <aliftriadi.gaming@gmail.com>
2025-11-01 14:12:40 +0700

    feat(category): add trigger to create default user categories when create user from admin

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -2,11 +2,24 @@
 
 namespace App\Filament\Resources\Users\Pages;
 
-use App\Filament\Resources\Users\Schemas\UserForm;
 use App\Filament\Resources\Users\UserResource;
+use App\Models\Category;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+        foreach (Category::get() as $category) {
+            $user->categories()->create([
+                'name' => $category->name,
+                'type' => $category->type,
+                'color' => $category->color,
+                'icon' => $category->icon,
+            ]);
+        }
+    }
 }
